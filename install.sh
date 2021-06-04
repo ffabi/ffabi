5787f9c49cf70d9168bb11bb131e9e01c63f0d25
Fabian <ffabi1997@gmail.com>
2021-06-04 13:31:06 +0200

    Fix

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+wget https://ffabi.github.io/ffabi/tmux.conf
+wget https://ffabi.github.io/ffabi/append_bashrc
 
 more append_bashrc >> ~/.bashrc
 source ~/.bashrc
+
+mv tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
